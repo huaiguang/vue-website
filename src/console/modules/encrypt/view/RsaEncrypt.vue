affd935de1084dab6457f402ea1a51cd47317992
chenjiaxin <chenjiaxin@lakala.com>
2020-12-15 18:21:01 +0800

    improve encrypt/decrypt

--- src/console/modules/encrypt/view/RsaEncrypt.vue
+++ src/console/modules/encrypt/view/RsaEncrypt.vue
@@ -2,6 +2,11 @@
   <div class="container">
     <h2>rsa加解密</h2>
     <el-row :gutter="10">
+      <el-col :span="24">
+        <div>生成随机数</div>
+        <el-input class="item-encrypt" type="textarea" v-model="randomDigit" placeholder="唯一的随机数"></el-input>
+        <el-button @click="createUniqueRandomNumber">随机数</el-button>
+      </el-col>
       <el-col :span="12">
         <div class="item-title">rsa 加密</div>
         <el-input class="item-encrypt" type="textarea" v-model="originalText" placeholder="请输入原文"></el-input>
@@ -9,7 +14,7 @@
       </el-col>
       <el-col :span="12">
         <div class="item-title">rsa 解密</div>
-        <el-input class="item-encrypt" type="textarea" v-model="encryptedText"></el-input>
+        <el-input class="item-encrypt" type="textarea" :rows="3" v-model="encryptedText"></el-input>
         <el-button class="item-encrypt" @click="decryptTextByRsa">rsa解密</el-button>
       </el-col>
     </el-row>
@@ -23,15 +28,16 @@ export default {
   name: 'RsaEncrypt',
   data() {
     return {
+      randomDigit: '',
       // 加密
       originalText: '',
       encryptedText: ''
     }
   },
-  created() {
-
-  },
   methods: {
+    createUniqueRandomNumber() {
+      this.randomDigit = Number(Math.random().toString().substr(2)).toString(36)
+    },
     encryptTextByRsa() {
       console.log(rsaEncrypt(this.originalText))
     },
